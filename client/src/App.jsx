82cc887cfafc8037b95bb09a5915436b25a879c8
sheikoodally <sheikoodally@hotmail.com>
2024-04-17 16:38:17 -0400

    changes

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-out' element={<SignUp />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
