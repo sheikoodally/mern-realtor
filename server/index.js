d8d7cc72fa10f9b6c52b73543933c43f3ef09c1a
sheikoodally <sheikoodally@hotmail.com>
2024-04-17 17:47:34 -0400

    setting server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'sheik',
+    database: 'realtor'
+}).promise()
+
+const [rows] = await pool.query("SELECT * from users");
+console.log(rows)
 
 const app = express();
 
 app.listen(3000, () => {
-    console.log('server is running!');
-
+    console.log('server is running!!!');
 })
